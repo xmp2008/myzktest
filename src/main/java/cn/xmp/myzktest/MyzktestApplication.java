b8aa61d35898364379ac48f36edae9536db5288f
xiemopeng <840392409@qq.com>
2022-06-28 15:33:52 +0800

    init

---
+++ src/main/java/cn/xmp/myzktest/MyzktestApplication.java
@@ -0,0 +1,13 @@
+package cn.xmp.myzktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyzktestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyzktestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xmp/myzktest/MyzktestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xmp.myzktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyzktestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
